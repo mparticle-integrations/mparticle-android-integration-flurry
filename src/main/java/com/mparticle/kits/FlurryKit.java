e04433cca5409e68aca8834da406dd90c362d67f
Sam Dozor <sdozor@mparticle.com>
2017-03-29 10:06:58 -0400

    Update to Flurry 6.9.2

--- src/main/java/com/mparticle/kits/FlurryKit.java
+++ src/main/java/com/mparticle/kits/FlurryKit.java
@@ -1,10 +1,8 @@
 package com.mparticle.kits;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.flurry.android.Constants;
@@ -13,12 +11,11 @@ import com.flurry.android.FlurryInstallReceiver;
 import com.mparticle.MPEvent;
 import com.mparticle.MParticle;
 
-import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class FlurryKit extends KitIntegration implements KitIntegration.AttributeListener, KitIntegration.EventListener, KitIntegration.ActivityListener {
+public class FlurryKit extends KitIntegration implements KitIntegration.AttributeListener, KitIntegration.EventListener {
     private static final String API_KEY = "apiKey";
     private static final String HASH_ID = "hashCustomerId";
     private static final String CAPTURE_EXCEPTIONS = "captureExceptions";
@@ -32,18 +29,20 @@ public class FlurryKit extends KitIntegration implements KitIntegration.Attribut
 
     @Override
     protected List<ReportingMessage> onKitCreate(Map<String, String> settings, Context context) {
+        boolean logEnabled = false;
         if (MParticle.getInstance().getEnvironment().equals(MParticle.Environment.Development)) {
-        FlurryAgent.setLogLevel(Log.VERBOSE);
-        FlurryAgent.setLogEnabled(true);
-    }else{
-        FlurryAgent.setLogEnabled(false);
-    }
+            logEnabled = true;
+        }
         if (getSettings().containsKey(INCLUDE_LOCATION) && !Boolean.parseBoolean(getSettings().get(INCLUDE_LOCATION))) {
             includeLocation = false;
         }
-        FlurryAgent.setReportLocation(includeLocation);
-        FlurryAgent.setCaptureUncaughtExceptions(Boolean.parseBoolean(getSettings().get(CAPTURE_EXCEPTIONS)));
-        FlurryAgent.init(getContext(), getSettings().get(API_KEY));
+
+        new FlurryAgent.Builder()
+                .withLogEnabled(logEnabled)
+                .withLogLevel(Log.VERBOSE)
+                .withCaptureUncaughtExceptions(Boolean.parseBoolean(getSettings().get(CAPTURE_EXCEPTIONS)))
+                .build(context, getSettings().get(API_KEY));
+
         return null;
     }
 
@@ -165,50 +164,5 @@ public class FlurryKit extends KitIntegration implements KitIntegration.Attribut
                         .setOptOut(optOutStatus)
         );
         return messageList;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityStopped(Activity activity) {
-        FlurryAgent.onEndSession(activity.getApplicationContext());
-        List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
-        messageList.add(
-                new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
-        );
-        return messageList;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityDestroyed(Activity activity) {
-        return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityStarted(Activity activity) {
-        FlurryAgent.onStartSession(activity.getApplicationContext());
-        List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
-        messageList.add(
-                new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
-        );
-        return messageList;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityResumed(Activity activity) {
-        return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityPaused(Activity activity) {
-        return null;
     }
 }
